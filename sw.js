00afb2d6fd841d92cbf6c718683fafc95ab50e0a
fernakasa <fernakasa@gmail.com>
2020-11-09 22:14:30 -0300

    v1
    v1

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 
 self.onmessage = function (obj) {
     fetch("https://gamedashboarducp.azurewebsites.net/api/game", {
-        method: 'POST', 
+        method: 'post', 
         header: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
-        body: JSON.stringify({"game": 'v1', "event": 'v1'})
+        body: JSON.stringify({game: "Bullet-Drizzle-Fernando", event: obj.data[0], data: ""})
     })};
