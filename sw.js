995002e288de4e9c6e64f00543c534d8e4c756e6
fernakasa <fernakasa@gmail.com>
2020-11-09 21:59:19 -0300

    v1
    v1

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 
-self.onmessage = function (event) {
+self.onmessage = function (obj) {
     fetch("https://gamedashboarducp.azurewebsites.net/api/game", {
         method: 'post', 
         header: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
-        body: JSON.stringify({"game": 'v1', "event": 'v1', "data": 'v1'})
+        body: JSON.stringify({"game": 'v1', "event": 'v1'})
     })};
