bc596e8f4d4be0f8c6e75dc58f598ae5e52c2af4
fernakasa <fernakasa@gmail.com>
2020-11-09 21:50:14 -0300

    v1
    v1

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-self.onmessage = function (obj) {
-    fetch("https://gamedashboarducp.azurewebsites.net/api/game", {
+fetch("https://gamedashboarducp.azurewebsites.net/api/game", {
         method: 'post', 
         header: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
           },
         body: JSON.stringify(obj)
-    })};
+    });
