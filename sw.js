e11e13991d50393eddbd08b26a5413badace4a17
fernakasa <fernakasa@gmail.com>
2020-11-09 21:16:53 -0300

    v4
    v4

--- sw.js
+++ sw.js
@@ -2,4 +2,4 @@ self.onmessage = function (event) {
     fetch("https://gamedashboarducp.azurewebsites.net/api/game", {
         method: 'post', 
         body: JSON.stringify(event)
-    })
+    })};
